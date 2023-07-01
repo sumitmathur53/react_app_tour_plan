2381c4414ec8b10d838de540a8839209a8562632
sumit kumar <sumitmathur170@gmail.com>
2023-07-02 01:16:04 +0530

    Tour app project with react

---
+++ src/components/Tours.js
@@ -0,0 +1,28 @@
+
+import React from 'react'
+import Card from './Card'
+
+export default function Tours({tours,removeTour}) {
+  return (
+
+    <div className='container'>
+
+      <div>
+      <h2 className='title'>Plane with Love</h2>
+      </div>
+
+      <div className='cards'>
+        {
+            tours.map( (tour)=>{
+
+                return <Card key={tour.id} {...tour} removeTour ={removeTour} ></Card>
+
+            })
+        }
+      </div>
+
+
+    </div>
+
+  )
+}
